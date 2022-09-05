a859c092950d8dc1391cf21365bebcd32f1ef3b1
SHYXIN <55689295+SHYXIN@users.noreply.github.com>
2022-09-05 11:28:09 +0800

    'model创建\数据库迁移\admin管理'

--- base/models.py
+++ base/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) # 可以为空
+    # participants = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) # 只有第一次自动当前时间
+    
+    
+    def __str__(self):
+        return self.name
+    
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) # 级联删除
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) # 只有第一次自动当前时间
+    
+    def __str__(self):
+        return self.body[0:50]
--- my_study_bud/settings.py
+++ my_study_bud/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'django.contrib.admin', # 管理员应用
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
