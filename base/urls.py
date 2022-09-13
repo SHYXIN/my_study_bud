daad02b29de899821cec6469350a2d5b4418f93a
SHYXIN <55689295+SHYXIN@users.noreply.github.com>
2022-09-13 10:08:00 +0800

    完成User Profile Page

--- base/urls.py
+++ base/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
     
     path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
+    path('user-profile/<str:pk>/', views.userProfile, name='user-profile'),
 ]
