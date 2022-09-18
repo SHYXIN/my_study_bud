26a6a9d6692379a08dbecc94ac36de6d82f6abd9
SHYXIN <55689295+SHYXIN@users.noreply.github.com>
2022-09-18 12:27:52 +0800

    后台不显示密码

--- base/admin.py
+++ base/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Room,Topic,Message, User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ['password']
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
